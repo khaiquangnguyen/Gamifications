395b0d5aa618b2ab9142703830d6f6df959f2cef
Khai Nguyen <kqn001@bucknell.edu>
2019-10-14 22:46:44 -0700

    fixed some weird configs

--- Timezoner/src/BaseComponents/Button/Button.tsx
+++ Timezoner/src/BaseComponents/Button/Button.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {btn} from './Button.module.scss';
+import styles from './Button.module.scss';
 interface ButtonProps {
     label? : string,
 }
 const Button: React.FC<ButtonProps> = ({label}) => {
     return (
         <button
-        className={btn}
+        className={styles.btn}
         onClick={() => alert('I am styled with CSS Modules')}
     >
         I am button 2 - Press You
