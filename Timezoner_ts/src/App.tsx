eb897c8d9e2fdf3a6f4684ad8fb28e376fc75ddf
Khai Nguyen <kqn001@bucknell.edu>
2019-10-24 14:47:46 -0700

    update git ignore

---
+++ Timezoner_ts/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App';
+
+const App: React.FC = () => {
+  console.log(styles);
+  return (
+    <div className='App'>
+    </div>
+  );
+};
+
+export default App;
