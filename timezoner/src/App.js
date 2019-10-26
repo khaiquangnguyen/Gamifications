997448c3c5b46e3322d6f84ff2a272ddb9a81095
Khai Nguyen <kqn001@bucknell.edu>
2019-10-25 21:00:11 -0700

    finished a temporary night theme

--- timezoner/src/App.js
+++ timezoner/src/App.js
@@ -3,34 +3,43 @@ import logo from './logo.svg';
 import styles from './App.module.scss';
 import Card from './Card/Card';
 import { GiConsoleController } from 'react-icons/gi';
-
+import HangAvatar from './Hang_avatar.jpg';
+import KhaiAvatar from './Khai_avatar.jpg'
+import KhoiAvatar from './Khoi_avatar.JPG';
 function App() {
   return (
     <div className={styles.Container}>
       <Card
-        thumbnail='https://semantic-ui.com/images/avatar/large/joe.jpg'
+        thumbnail={KhoiAvatar}
         isMale={true}
         name='Khoi Le'
-        role={<GiConsoleController size='2rem'/>}
+        role={<GiConsoleController size='1.5rem' color='#537780'/>}
         timeZone='Asia/Bangkok'
         key='Khoi Le'
+        country='VN'
+        countryName='Viet Nam'
       />
 
       <Card
-        thumbnail='https://semantic-ui.com/images/avatar/large/chris.jpg'
+        thumbnail={KhaiAvatar}
         isMale={true}
         name='Khai Nguyen'
-        role={<GiConsoleController size='2rem'/>}
+        role={<GiConsoleController size='1.5rem' color='#537780'/>}
         timeZone='America/Los_Angeles'
         key='Khai Nguyen'
+        country='US'
+        countryName = 'United States'
+
       />
       <Card
-        thumbnail='https://semantic-ui.com/images/avatar/large/ade.jpg'
+        thumbnail={HangAvatar}
         isMale={false}
         name='Hang Ha'
-        role={<GiConsoleController size='2rem'/>}
+        role={<GiConsoleController size='1.5rem' color='#537780'/>}
         timeZone='Europe/Zurich'
         key='Hang Ha'
+        country='GB-ENG'
+        countryName = 'England'
       />
 
     </div>
--- timezoner/src/Card/Card.js
+++ timezoner/src/Card/Card.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Card.module.scss';
 import PropTypes from 'prop-types';
 import {GiMale, GiFemale} from 'react-icons/gi';
+import { FlagIcon } from "react-flag-kit";
 
 const propTypes = {
   thumbnail: PropTypes.string.isRequired,
@@ -12,13 +13,31 @@ const propTypes = {
   role: PropTypes.string.isRequired,
   flag: PropTypes.string.isRequired,
   rating: PropTypes.string.isRequired,
+  countryName: PropTypes.string.isRequired,
 };
 
 const defaultProps = {
 
 };
 
-const Card = ({thumbnail, timeZone, name, country, isMale, role, flag, rating}) => {
+const Card = ({thumbnail, timeZone, name, country, isMale, role, rating, countryName}) => {
+  const stars = (() => {
+    const stars = [];
+    for (let i = 0; i <= 50; i++) {
+      const radius= `${Math.ceil(Math.random()+2)}px`;
+
+      const divStyle = {
+        top: `${Math.ceil(Math.random() * 100)}%`,
+        left:`${Math.ceil(Math.random() * 100)}%`,
+        width:radius,
+        height: radius,
+      };
+      stars.push(<span key={i} className={styles.Star} style={divStyle}/>)
+
+    }
+    return stars;
+  })();
+
   const timeOption = {
     timeZone:timeZone,
     hour: '2-digit',
@@ -35,27 +54,34 @@ const Card = ({thumbnail, timeZone, name, country, isMale, role, flag, rating}) 
   console.log(timeZone,time, date);
   return (
     <div className={styles.Container}>
+      {stars}
       <div className={styles.Header}>
-        <div className={styles.Flag}/>
-        <div className={styles.Rating}/>
+        <div className={styles.Flag}>
+          <FlagIcon code={country} size={32} />
+        </div>
+        <div className={styles.CountryName}>
+          {countryName}
+        </div>
       </div>
       <div className={styles.ThumbnailSection}>
         <img className={styles.Avatar} src={thumbnail} alt={name} />
-        <div className={styles.Gender}>
-          {isMale? <GiMale size='2rem'/> : <GiFemale size='2rem'/>}
-        </div>
-        <div className={styles.RoleType}>
-          {role}
-        </div>
-        </div>
+        {/*<div className={styles.Gender}>*/}
+        {/*  {isMale? <GiMale size='1.5rem' color='#537780'/> : <GiFemale size='1.5rem' color='#537780'/>}*/}
+        {/*</div>*/}
+        {/*<div className={styles.RoleType}>*/}
+        {/*  {role}*/}
+        {/*</div>*/}
+      </div>
+      <div className={styles.NameTimeContainer}>
       <div className={styles.Name}>
         {name}
       </div>
       <div className={styles.TimeZoneBox}>
-        <span>{time} </span>
-        <span> {date} </span>
+        <span className={styles.Time}>{time} </span>
+        <span className={styles.Date}> {date} </span>
       </div>
-      <div className={styles.TimeIllustration} />
+      </div>
+      {/*/!*<div className={styles.TimeIllustration} />*!/*/}
     </div>)
 };
 
